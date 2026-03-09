9b77eae2d5c7085bc62d36a8b76b07a2c16d72ae
Mohammad Noureddine <nouredd2@users.noreply.github.com>
2026-03-09 12:46:19 -0400

    Lab 01: Added a debug version of the hello program to make in-class debugging easier

---
+++ Lab01/tests/hello_debug.c
@@ -0,0 +1,20 @@
+/************************************************************************************
+ *
+ * Copyright (c) 2025 Rose-Hulman Institute of Technology. All Rights Reserved.
+ *
+ * Should you find any bugs in this file, please contact your instructor as
+ * soon as possible.
+ *
+ ***********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hello.h"
+
+int
+main(int argc, char **argv)
+{
+  print_hello_world();
+
+  return EXIT_SUCCESS;
+}
--- Lecture_Demos/c_review/pointers.c
+++ Lecture_Demos/c_review/pointers.c
@@ -8,5 +8,8 @@ main(int argc, char **argv)
   // silence compilation warnings, ignore this.
   (void)p;
 
+  printf("p contains the address %p\n", p);
+  printf("What would be this value? %p\n", &p);
+
   return 0;
 }
